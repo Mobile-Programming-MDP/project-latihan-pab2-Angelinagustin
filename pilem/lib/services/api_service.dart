89296c4067b1011967db6afab6abb4e2925f8829
Angelinagustin <angelinagustin_2226240035@mhs.mdp.ac.id>
2025-02-26 19:31:26 +0700

    Add: api service

---
+++ pilem/lib/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '85e637b0ad9fb4868a4f624c41bad5a9';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response =
+        await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http
+        .get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response =
+        await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+}
