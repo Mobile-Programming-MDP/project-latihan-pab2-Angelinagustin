69a6e49f590ed7cb52ebcbc34af1cd8b1187d1ca
Angelinagustin <160854942+Angelinagustin@users.noreply.github.com>
2025-03-28 14:34:45 +0700

    Add: main & home screen

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -1,15 +1,36 @@
+import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State <HomeScreen> createState() => _HomeScreenState();
-}
+  Future<void>signout(BuildContext context)async{
+    await FirebaseAuth.instance.signOut();
 
-class _HomeScreenState extends State <HomeScreen> {
+
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context)=>SignInScreen())
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+    appBar: AppBar(
+      title: const Text('Home'),
+      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      actions: [
+        IconButton(onPressed: (){
+          signout(context);
+        },
+        icon: const Icon(Icons.logout),
+        )
+      ],
+    ),
+    body: const Center(
+      child: Text('you have logged in'),
+    ),
+  );
   }
 }
